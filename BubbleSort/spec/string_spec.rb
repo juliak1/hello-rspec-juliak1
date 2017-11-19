c3b003a81949a565c9fc84ced78111ce3896b2d1
Julia <julia@email.com>
2017-11-19 21:41:23 +0100

    Fixes

--- BubbleSort/spec/string_spec.rb
+++ BubbleSort/spec/string_spec.rb
@@ -16,6 +16,16 @@ RSpec.describe '../lib/bubble_sort.rb' do
       values = ['zxc', 'xcvb', 'gfdsa', 'mnods', 'asdfg', 'lpdfg']
       expect(bubble_sort(values)).to match(['asdfg', 'gfdsa', 'lpdfg', 'mnods', 'xcvb', 'zxc'])
     end
+
+    it 'returns sorted array of six different type values' do
+      values = ['bvx', 'aasv', '5', '2', 'xca', '@']
+      expect(bubble_sort(values)).to match(["2", "5", "@", "aasv", "bvx", "xca"])
+    end
+
+    it 'returns sorted array of ten different type values' do
+      values = ['bvfsds', 'abcdef', '12', '16.2123', '11.1233', 'fghasd', 'qwerty', 'lpkjihyu', 'asdfgh', 'mnbvc']
+      expect(bubble_sort(values)).to match(["11.1233", "12", "16.2123", "abcdef", "asdfgh", "bvfsds", "fghasd", "lpkjihyu", "mnbvc", "qwerty"])
+    end
    
   end
 end
